9f10cf5be2131b3daad79703b3dc470fb053ae71
nibill <tet@tes.ch>
2019-04-02 11:24:36 +0200

    assignment 2

--- assignments/toolcalibration/calibration.py
+++ assignments/toolcalibration/calibration.py
@@ -11,4 +11,24 @@ def pivot_calibration(transforms):
     p_t = np.zeros((3, 1))
     T = np.eye(4)
 
+    A = []
+    b = []
+
+    for item in transforms:
+        i = 1
+        A.append(np.append(item[0, [0, 1, 2]], [-1, 0, 0]))
+        A.append(np.append(item[1, [0, 1, 2]], [0, -1, 0]))
+        A.append(np.append(item[2, [0, 1, 2]], [0, 0, -1]))
+        b.append((item[0, [3]]))
+        b.append((item[1, [3]]))
+        b.append((item[2, [3]]))
+
+    x = np.linalg.lstsq(A, b, rcond=None)
+
+    result = (x[0][0:3]).flatten() * -1
+    #result = [result]
+
+    p_t = np.asarray(result).transpose()
+    T[:3, 3] = p_t.T
+
     return p_t, T
